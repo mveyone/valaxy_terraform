607c46fa90aa2711d3eda609333313f4f1cbe135
mveyone <mveyone@outlook.fr>
2023-06-20 00:27:59 +0100

    fix vars regions

--- variables.tf
+++ variables.tf
@@ -9,16 +9,15 @@ variable "ami" {
 }
 
 variable "vpc_cidr" {
-  type = string
-  default = "10.0.0.0/16"
+    default = "10.10.0.0/16"
 }
 
 variable "subnet_1_cidr" {
-  type = string
-  default = "10.0.1.0/24"
+ 
+  default = "10.10.1.0/24"
 }
 
 variable "subnet_az" {
   type = string
-  default = "us-east-1e"
+  default = "us-east-1a"
 }
