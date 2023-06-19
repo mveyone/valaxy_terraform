895dd457410b0f35257bc84a482035c3223aeef1
mveyone <mveyone@outlook.fr>
2023-06-20 00:21:29 +0100

    fix

--- vpc_with_ec2.tf
+++ vpc_with_ec2.tf
@@ -17,7 +17,9 @@ resource "aws_vpc" "demo-vpc" {
 //create subnet
 resource "aws_subnet" "demo-subnet" {
   vpc_id     = aws_vpc.demo-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_1_cidr
+  map_public_ip_on_launch = true
+  availability_zone = var.subnet_1_cidr
 
   tags = {
     Name = "demo-subnet"
