e847a1d5263b6b37f39ebd467b4e756026c09797
mveyone <mveyone@outlook.fr>
2023-06-20 00:17:43 +0100

    vars

--- variables.tf
+++ variables.tf
@@ -1,9 +1,24 @@
 variable "region" {
   type = string
-  default = "us-east-1"
+  default = "us-east-1d"
 }
 
 variable "ami" {
   type=string
   default = "ami-053b0d53c279acc90"
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_1_cidr" {
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "subnet_az" {
+  type = string
+  default = "us-east-1d"
 }
--- vpc_with_ec2.tf
+++ vpc_with_ec2.tf
@@ -11,13 +11,15 @@ resource "aws_instance" "Ubuntu" {
 
 //create vpc
 resource "aws_vpc" "demo-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 }
 
 //create subnet
 resource "aws_subnet" "demo-subnet" {
   vpc_id     = aws_vpc.demo-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_1_cidr
+  map_public_ip_on_launch = true
+  availability_zone = var.subnet_1_cidr
 
   tags = {
     Name = "demo-subnet"
